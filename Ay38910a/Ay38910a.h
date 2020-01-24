8116c5328b1a43eb568d60318981dc72af5fb148
mar <gim.marcello@gmail.com>
2020-01-24 10:38:12 +0100

    Update Ay38910a.h

--- Ay38910a/Ay38910a.h
+++ Ay38910a/Ay38910a.h
@@ -1,8 +1,20 @@
 /*
 
   Ay38910a.h library containing functions that make easier to interact
-  with the Ay38910 sound chip
+  with the Ay38910 sound chip. This is an adaptation of a Matio Klingemann's skecth; the
+  original can be found at:
+  https://github.com/Quasimondo/Arduino-Sketches/blob/master/AY-3-8910%20Sound%20Chip/Sound%20FX/AY38910_fx.ino
+  
+  Below is the original description of the sketch.
+  
+  ***
+  No Quarz required version for controlling a AY-3-8910 sound chip with the Arduino
+  Author: Matio Klingemann http://incubator.quasimondo.com
 
+  A bomb drop sound effect modeled after an example found here
+  http://books.google.de/books?id=ocjJE4KtZq4C&lpg=PA95&dq=ay-3-8910&pg=PA90#v=onepage&q=ay-3-8910&f=false
+  ***
+  
 */
 
 #ifndef Ay38910a_h
